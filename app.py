edb59a0b9908583cd4f709e527d7845830b35bbd
GeorhiiIakimenko <workyakimenko@yahoo.com>
2024-03-25 10:53:51 +0200

    1

--- app.py
+++ app.py
@@ -1,18 +1,35 @@
+import base64
+from io import BytesIO
 from flask import Flask, request, jsonify
 from flask_restful import Api, Resource
 from flasgger import Swagger
 import torch
 from diffusers import StableDiffusionXLPipeline
+from PIL import Image
+import torchvision.transforms as transforms
 
 app = Flask(__name__)
 api = Api(app)
-swagger = Swagger(app, template_file='swagger.yaml')
-
+swagger = Swagger(app, template_file='swagger.yml')
 
 # Load Stable Diffusion model
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = StableDiffusionXLPipeline.from_pretrained("segmind/SSD-1B").to(device)
 
+def preprocess_image(image_data):
+    # Преобразование байтов изображения в объект PIL.Image
+    image = Image.open(BytesIO(image_data))
+
+    # Применение необходимых преобразований
+    preprocess = transforms.Compose([
+        transforms.Resize(256),  # Масштабирование изображения до 256x256
+        transforms.CenterCrop(224),  # Обрезка изображения до 224x224 по центру
+        transforms.ToTensor(),  # Преобразование изображения в тензор
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Нормализация значений пикселей
+    ])
+
+    # Применение преобразований к изображению
+    return preprocess(image).unsqueeze(0)  # Добавление размерности пакета
 
 class ProcessImage(Resource):
     def post(self):
@@ -35,21 +52,26 @@ class ProcessImage(Resource):
           200:
             description: Processed image
         """
-        # Get image data and text prompt from request
+        # Получаем данные изображения и текстовый промпт из запроса
         image_data = request.files['image'].read()
         prompt = request.form['prompt']
 
+        # Преобразуем данные изображения в тензор
+        image_tensor = preprocess_image(image_data).to(device)
+
         # Process image using Stable Diffusion model
         with torch.no_grad():
-            # Your image processing code here
-            processed_image = model(image_data, prompt)
+            # Обработка изображения с использованием модели
+            processed_image = model(prompt=prompt, image=image_tensor).images[0]
 
-        # Return processed image in JSON format
-        return jsonify({'processed_image': processed_image})
+        # Возвращение обработанного изображения в формате base64
+        buffered = BytesIO()
+        processed_image.save(buffered, format="JPEG")
+        processed_image_base64 = base64.b64encode(buffered.getvalue()).decode('utf-8')
+        return jsonify({'processed_image': processed_image_base64})
 
 
 api.add_resource(ProcessImage, '/api/main')
-api.init_app(app)
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000, host="127.0.0.1")
